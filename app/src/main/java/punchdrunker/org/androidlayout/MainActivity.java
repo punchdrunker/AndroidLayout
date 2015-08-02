fcd608ea2faef54cca7ff21378ba86cdc55d3296
punchdrunker <nanao.hhk@gmail.com>
2015-08-03 00:23:09 +0900

    余白とか、インデントとか、その他もろもろ

--- app/src/main/java/punchdrunker/org/androidlayout/MainActivity.java
+++ app/src/main/java/punchdrunker/org/androidlayout/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,16 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button jp_button = (Button) findViewById(R.id.my_button);
+        final TextView msg_text = (TextView) findViewById(R.id.my_text);
+
+        jp_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                msg_text.setText("さようなら");
+            }
+        });
+
     }
 
     @Override
